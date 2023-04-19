9922622d121943dd7d1bb3425cbc37790a31e4f1
Vita Fomin <vita.bukhalo@gmail.com>
2023-04-19 15:20:25 -0400

    added some questions, packages, and started the functions

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const markdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What would you like to title your README?", "What wouold you like to include in your decription section?", "What would you like to include in intallation section?", "What would you like to include in your usage section?", "Who would you like to include in your credits section?", "What license would you like to include?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  license = license.toUpperCase().trim();
+  if (license == 'MIT') {
+    return 
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
