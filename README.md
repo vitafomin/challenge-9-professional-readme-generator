b01e5cb0accf67eabf7ae05d3d385ed2f5547138
Vita Fomin <vita.bukhalo@gmail.com>
2023-04-20 14:46:20 -0400

    added prompt questions and created a fuction to generate a readme file from the users input

--- README.md
+++
@@ -1,1 +0,0 @@
-# challenge-9-professional-readme-generator
--- index.js
+++ index.js
@@ -4,13 +4,122 @@ const inquirer = require("inquirer");
 const markdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = ["What would you like to title your README?", "What wouold you like to include in your decription section?", "What would you like to include in intallation section?", "What would you like to include in your usage section?", "Who would you like to include in your credits section?", "What license would you like to include?"];
+const questions = ["What is the title of your README?", "What wouold you like to include in your decription section?", "What would you like to include in intallation section?", "What would you like to include in your usage section?", "Who would you like to include in your credits section?", "What license would you like to include?", "Please include testing insructions", "Please include any questions"];
 
+const generateReadMe = ({title, description, installation, usage, githubName, credits, license, test, email}) =>
+
+`# ${title}
+
+## Table of Contents
+Description
+Installation
+Usage
+Credits 
+License 
+Test
+Questions
+
+## Description
+
+${description}.   
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+https://${githubName}.github.io/${title}/
+
+## Credits
+
+${credits}
+
+## License
+
+${license}
+
+## Test 
+
+${test}
+
+## Questions
+
+${githubName}
+https://github.com/${githubName}
+
+Please contact me via email with any additional questions that you may have:
+${email}
+`
+
+inquirer
+  .prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What would you like to title your README"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What wouold you like to include in your decription section?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What would you like to include in intallation section?" 
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What would you like to include in your usage section?" 
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "Who would you like to include in your credits section?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like to include?",
+        choices: ["MIT", "Apache", "BSD", "Creative Commons", "GNU"]
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please include testing insructions", 
+    },
+    {
+        type: "input",
+        name: "githubName",
+        message: "please provide your github username" 
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "please provide your email address"
+    }
+  ])
+  .then((answers) => {
+    console.log(answers)
+    fs.writeFile("README.md", generateReadMe(answers), (err) => err ? console.log(err) : console.log("Professional README created!"))
+    // const readmePage = generateReadMe(answers);
+  })
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     fs.writeFile("README.md", readmePage, (err) => 
+//     err ? console.log(err) : console.log("Professional README created!"));
+// };
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {
+//     prompt()
+//     .then((answers) => writeFile("README.md", generateReadMe(answers)))
+//     .then(() => console.log("Professional README created!"))
+//     .catch((err) => console.error(err));
+// };
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
